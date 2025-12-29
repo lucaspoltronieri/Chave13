5e66028f823db6c1a900e92f58ddba46fc6944e0
Lucas Poltronieri <lucaspoltronierisampaio@gmail.com>
2025-12-29 16:55:29 -0300

    Initial commit Create project Chave13 Erp for Oficinas

---
+++ src/main/java/com/chave13/erp/Chave13Application.java
@@ -0,0 +1,13 @@
+package com.chave13.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chave13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chave13Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chave13/erp/Chave13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chave13.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chave13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
